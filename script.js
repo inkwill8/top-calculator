57247cbd88b195d6028b1182040bc83965df8c9c
inkwill8 <inkwill8@proton.me>
2025-05-16 14:46:11 -0400

    Add JavaScript comments

---
+++ script.js
@@ -0,0 +1,18 @@
+// Operator functions go here:
+	// Addition function
+	// Subtraction function
+	// Multiplication function
+	// Division function
+
+// Operations Variables:
+	// Operand 1
+	// Operation
+	// Operand 2
+
+// OPERATE FUNCTION
+	// Takes an operation and 2 numbers, then calls
+	// one of the above functions on those numbs.
+
+// DOM Elements
+
+// POPULATE DISPLAY FUNCTION
