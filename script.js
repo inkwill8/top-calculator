37973c541506b65eafefc5cfa25d5adefd1629cd
inkwill8 <inkwill8@proton.me>
2025-05-20 19:54:05 -0400

    AppendToDisplay function actually does something!

--- script.js
+++ script.js
@@ -32,10 +32,33 @@
 	// Operand 2
 		let secondOperand = null;
 
+
+
+
 // OPERATE FUNCTION
 	// Takes an operation and 2 numbers, then calls
 	// one of the above functions on those numbs.
 
+	//function operate(operation, firstOperand, secondOperand) {
+		//operation(firstOperand, secondOperand)  
+
 // DOM Elements
+	const display = document.querySelector('#display');
+	const clearBtn = document.querySelector('#all-clear');
+	const squareBtn = document.querySelector('#square');
+	const operatorBtns = document.querySelectorAll('.operator');
+	const oneThruNineBtns = document.querySelectorAll('.digit');
+	const zeroBtn = document.querySelector('#zero');
+	const decimalBtn = document.querySelector('#point');
+	const equalBtn = document.querySelector('#equal');
 
 // POPULATE DISPLAY FUNCTION
+	function appendToDisplay(e) {
+		display.value = e.target.textContent;
+	};
+
+// EVENT LISTENERS
+	//clearBtn.addEventListener('click', clearDisplay);
+	zeroBtn.addEventListener('click', appendToDisplay);
+	squareBtn.addEventListener('click', appendToDisplay);
+
